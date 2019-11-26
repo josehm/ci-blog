51339fc158691ffde39ed9c72e28247a5b1d0200
Jose <jose@stetamalo.com>
2019-11-26 12:29:42 -0600

    Change data for jose

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
+/*
 | Service        | Run Time           | Configuration Time           | 
 | ------------- |:-------------:| :-------------:|
 | CircleCI      | 2 min | 1 hour |
@@ -13,3 +13,4 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 | Travis      | 3 min 7 seconds | 1 hour |
 | Buddy | 1 min      |  10 minutes |  
 | AWS CodePipeline | 3 min 14 seconds      | 30 minutes | 
+*/
